5701ae45f1e9366bcbd42d34a53f9df72f1459ff
nurfitriakh <nurfitriakh@gmail.com>
2022-10-12 17:56:22 +0700

    hilang plotlib coba

--- DA.py
+++ DA.py
@@ -2,7 +2,7 @@
 # import libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import seaborn as sns
 import streamlit as st
 from sklearn import metrics

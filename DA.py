9ef9a4bfb866c690ce4e2b85cd9934205525b081
nurfitriakh <nurfitriakh@gmail.com>
2022-10-12 18:19:28 +0700

    line chart

--- DA.py
+++ DA.py
@@ -54,10 +54,8 @@ hour_df['hour'] = hour_df.hour.astype('category')
 
 
 st.dataframe(hour_df)
-st.table(hour_df)
-
-st.metric("Hum", 10, 4)
-st.line_chart(hour_df['Hum'])
+#st.table(hour_df)
+st.line_chart(hour_df)
 # %%
 """
 # plotting
